07af44f25083be21d32fb586aa6b98269ad7bd9b
Kader198 <maguiskader0@gmail.com>
2023-03-16 23:53:59 +0000

    gcest bien 80% mais reste les tests :Â)

--- resources/js/Pages/ImagesScore.jsx
+++ resources/js/Pages/ImagesScore.jsx
@@ -1,9 +1,12 @@
 import ImageSide from "@/Components/ImageSide";
-
+import CatImage from "@/Assets/images/catmash1.jpg";
 export default function ImagesScore(props) {
     return (
         <section className='py-14'>
             <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+                {/*<div className={'text-center place-content-center'}>*/}
+                {/*    <img src={CatImage} alt={"a"}/>*/}
+                {/*</div>*/}
                 <div className='relative max-w-xl mx-auto sm:text-center'>
                     <h3 className='text-gray-800 text-3xl font-semibold sm:text-4xl'>
                         Catsmash
